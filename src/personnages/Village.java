1121905721f4803e264b3f8242094d5e94019e23
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-15 16:11:23 +0200

    Update Village.java
    mise à jour

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -4,11 +4,12 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
-	private villageois = [];
-	private int nbVillageois;
+	private Gaulois[] villageois ;
+	private int nbVillageois = 0;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
 	}
 
 	public void setChef(Chef chef) {
@@ -18,4 +19,33 @@ public class Village {
 	public String getNom() {
 		return nom;
 	}
+	
+	public void ajouterHabitant(Gaulois villageois) {
+		this.villageois[nbVillageois] = villageois;
+		nbVillageois +=1;
+	}
+	
+	public Gaulois trouverHabitant(int nbVillageois) {
+		return villageois[nbVillageois];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef" + chef + "vivent les légendaires gaulois :");
+		for(int i =0; i <=nbVillageois; i++) {
+			System.out.println(" -" + trouverHabitant(i).getNom() + ",");
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles",30);
+		// System.out.println(village);
+		Gaulois Abraracourcix = new Gaulois("Abraracourcix",6);
+		// Gaulois Gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		Gaulois Obelix = new Gaulois("Obelix", 25);
+		Obelix.ajouterHabitant(Obelix);
+		village.afficherVillageois();
+	}
+	
 }
