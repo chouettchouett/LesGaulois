58220a0e6506eb98fa2e4d2840e70df3acfaf04c
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-25 17:24:35 +0200

    Debut

---
+++ src/personnages/Trophee.java
@@ -0,0 +1,19 @@
+package personnages;
+
+public class Tropheee{
+  private Gaulois gaulois;
+  private Equipement[] equipement; 
+  
+  
+  
+  public String donnerNom(){
+    return gaulois.geNom();
+  }
+  
+  
+}
+
+public class Musee{
+  private musee[200];
+  private int nbTrophee;
+  
