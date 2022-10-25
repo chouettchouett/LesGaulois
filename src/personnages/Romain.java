8e212c98b81ac89362282f7dcf3cb82d585f1f3e
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-25 13:16:07 +0200

    Romainpourtp3

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -68,7 +68,52 @@ public class Romain {
 		return  equipementEjecte;
 	}
 	
+	private int calculResistanceEquipement(int forceCoup){
+		String texte;
+		texte = "Ma force est de " + this.force + ", et la dorce du coup est de "+ forceCoup;
+		int resistanceEquipement = 0;
+		if (nbEquipement != 0) {
+			texte += "\n Mais heureusement, grâce à mon équipement sa force est diminué de ";
+			for (int i = 0; i < nbEquipement; i++){
+				if (equipements[i] != null && equipements[i].equals(Equipement.BOUCLIER)){
+					resistanceEquipement += 8;
+				}
+				else {
+					System.out.println("Equipement casque");
+					resistanceEquipement += 5
+					}
+			}
+			texte += resistanceEquipement + "!";
+		}
+		parler(texte);
+		forceCoup -= resistanceEquipement;
+		return forceCoup;
+	}
 	
+	private Equipement[] ejecterEquipement(){
+		Equipement[] equipementEjecte = new Equipement[nbEquipements];
+		System.out.println("L'Equipement de " + nom + " s'envole sous la force du coup.");
+		int nbEquipementEjecte = 0;
+		for (int i = 0; i < nbEquipement; i ++) {
+			if (equipements[i] != null) {
+				equipementEjecte[nbEquipementEjecte] = equipements[i];
+				nbEquipementEjecte++;
+				equipements[i] = null;
+			}
+		}
+		return equipementEjecte;
+		}
+	
+	public void presente(String text) {
+		System.out.println("Le soldat " + nom + text + ".");
+	}
+	
+	public void equipe(Equipement equip) {
+		presente("s'equipe avec un " + equip.getNom() );
+		nbEquipement ++;
+		equipements[nbEquipement-1]=equip;
+	}
+			
 	
 	public void sEquiper(Equipement equip) {
 		switch (nbEquipement){
@@ -86,14 +131,20 @@ public class Romain {
 			case 0:
 				equipe(equip);
 				break;
-				
-			
+		}
+	}
 	
+	@Override
+	public String toString(){
+		return "Romain [nom = " + nom +", force = " + force + "]";
+	}
 	
 	public static void main(String[] args) { 
-		 Romain Minus = new Romain("Minus", 6);
-		 //Minus.parler("ahhhhhh");
-		 //Minus.recevoirCoup(8); 	
-		 System.out.println("Test" + Equipement);
+		Romain Minus = new Romain("Minus", 6);
+		minus.nom = "minus";
+		minus.force = 6;
+		//Minus.parler("ahhhhhh");
+		//Minus.recevoirCoup(8); 	
+		System.out.println("Test" + Equipement);
 	 }
 }
