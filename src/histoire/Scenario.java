f62351160db8a320c1b2c815d4583daf7a4777b1
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-25 16:30:12 +0200

    Scenariopourtp3

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -3,17 +3,21 @@ package personnages;
 public class Scenario {
 	
 	public static void main(String[] args) {
-		Druide panoramix = new Druide("panoramix",5,10);
+		Druide panoramix = new Druide("Panoramix",5,10);
 		Gaulois asterix = new Gaulois("Astérix", 10);
-		Romain Minus = new Romain("Minus", 8);
+		Gaulois Obélix = new Gaulois("Obélix", 25);
+		Romain Minus = new Romain("Minus", 6);
 		panoramix.parler("Je vais aller préparer une petite potion..." );
-		Obelix.parler("Par Bénélos, ce n'est pas juste!");
+		panoramix.preparerPotion();
+		panoramix.booster(Obélix);
+		Obélix.parler("Par Bénélos, ce n'est pas juste!");
+		panoramix.booster(asterix);
 		asterix.parler("Bonjour à tous");
 		Minus.parler("UN GAU... UN GAUGAU");
-		asterix.frapper(Minus);
-		asterix.frapper(Minus);
-		asterix.frapper(Minus);
 		
+		while (minus.getForce() > 0) {
+			asterix.frapper(minus);
+		}
 	}
 
 }
