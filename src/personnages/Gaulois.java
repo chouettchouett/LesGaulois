f78ffb9adae9a5759dcdc5a5225c51fe60a7d906
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-25 10:54:26 +0200

    Gauloispourtpt3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -6,6 +6,8 @@ package personnages;
 	 private String nom; 
 	 private int force; 
 	 private int effetPotion = 1; 
+	 private int nbTrophees;
+	 private Equipements[] trophees = new Equipement[100];
 	 
 	 public Gaulois(String nom, int force) { 
 		 this.nom = nom; 
@@ -15,28 +17,48 @@ package personnages;
 		 return nom; 
 	 } 
 	 public void parler(String texte) { 
-		 System.out.println(prendreParole() + "« " + texte + " »"); 
+		 System.out.println(prendreParole() + "Â« " + texte + " Â»"); 
 	 } 
+//	 private String prendreParole() { 
+//		 return "Le gaulois " + nom + " : "; 
+	 
 	 private String prendreParole() { 
 		 return "Le gaulois " + nom + " : "; 
 	 } 
+//	 public void frapper(Romain romain) { 
+//		 System.out.println(nom + " envoie un grand coup dans la mÃ¢choire de " 
+//				 + romain.getNom()); 
+//		 romain.recevoirCoup(force / 3); 
+//	 } 
+	 
 	 public void frapper(Romain romain) { 
-		 System.out.println(nom + " envoie un grand coup dans la mâchoire de " 
+		 System.out.println(nom + " envoie un grand coup dans la mÃ¢choire de " 
 				 + romain.getNom()); 
-		 romain.recevoirCoup(force / 3); 
+		 trophees = romain.recevoirCoup((force / 3)* effetPotion);
+		 for(int i = 0; trophees != null && i < trophees.length; i++, nbTrophees++){
+			 this.trophees[nbTrophees] = trophees[i];
 	 } 
 	 
-	 @Override 
-	 public String toString() { 
-		 return "Gaulois [nom=" + nom + ", force=" + force 
-				 + ", effetPotion=" + effetPotion + "]"; 
-	 } 
+	 //@Override 
+	 //public String toString() { 
+	 //	 return "Gaulois [nom=" + nom + ", force=" + force 
+	//			 + ", effetPotion=" + effetPotion + "]"; 
+	 //} 
+		 
+	 public void boirePotion(int forcePotion){
+		 effetPotion += forcePotion;
+		 parler("Merci Druide, je sens que ma force est " + forcePotion + " fois decuplee.");
+	 }
+		 
 	 public static void main(String[] args) { 
-		 Gaulois asterix = new Gaulois("Astérix", 8);
+		 Gaulois asterix = new Gaulois("AstÃ©rix", 8);
+		 System.out.println(asterix.nom); //
+		 System.out.println(asterix.prendreParole()); //
 		 Romain Minus = new Romain("Minus", 8);
 		 System.out.println(asterix);
 		 asterix.parler("blabla");
 		 asterix.frapper(Minus);
+		 asterix.boirePotion(6);
 	 } 
 	 
  }
