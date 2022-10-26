d3f22540efce20edfc44bb412d1ff830cecc9a94
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-10-26 12:34:28 +0200

    tropheev2

--- src/personnages/Trophee.java
+++ src/personnages/Trophee.java
@@ -17,3 +17,16 @@ public class Musee{
   private musee[200];
   private int nbTrophee;
   
+  public void donnerTrophee( String gaulois, equipement){
+    musee += nbTrophee[equipement];
+  }
+  
+  public void faireUneDonation(String musee){
+    gaulois.donnerTrophee;
+    gaulois.parler("Je donne au musee tous mes trophees : ")
+    for( int i = 0, i < nbTrophee, i ++){
+      parler( equipement);
+    }
+  }
+    
+      
